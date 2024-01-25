3ac8268fc610f1bc60e44c6b19a8a69594e595b4
Dmitriy Yudichev <dimarik34rus@ya.ru>
2024-01-26 00:08:43 +0300

    update profile

---
+++ js/libs/live-chat.js
@@ -0,0 +1,49 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  //! Start Scripts
+
+  const liveChat = document.querySelector(".live-chat");
+  const liveChatClose = document.querySelector(".live-chat__close");
+
+  // закрытие чата
+  liveChatClose.addEventListener("click", function () {
+    liveChat.classList.remove("active");
+  });
+
+  // Открытия чата по кнопкам чата
+  const chatOpenBtns = document.querySelectorAll(".chat-open");
+  const privatGallery = document.querySelector(".gallery-private");
+
+  chatOpenBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+      liveChat.classList.add("active");
+      privatGallery.classList.remove("active");
+      document.body.style.overflow = "";
+      document.body.style.width = "";
+    });
+  });
+
+  // Открытия чата по кнопкам чата
+  const historyItem = document.querySelectorAll(".chat-history__item");
+
+  historyItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      liveChat.classList.add("active");
+    });
+  });
+
+  // EmojiPicker для liveChat
+  const emojiPickerLiveChat = new EmojiPicker({
+    trigger: [
+      {
+        selector: "#smile-livechat",
+        insertInto: ".live-chat__textarea",
+      },
+    ],
+    closeButton: true,
+    position: "topLeft",
+    // specialButtons: green
+  });
+
+  //!! End Scripts
+});
--- js/libs/my-gallery.js
+++ js/libs/my-gallery.js
@@ -5,13 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
   /* ------------------- My Gallery -------------------- */
   const photoItems = document.querySelectorAll(".private-photo");
   const myGallery = document.querySelectorAll(".my-gallery");
+  const bodyPage = document.body;
 
-  // Функция для отображения галереи
-  function showGallery(galleryId, startIndex) {
-    const gallery = document.querySelector(`[data-gallery="${galleryId}"]`);
+  function showGallery(gallery, startIndex) {
     const galleryItems = gallery.querySelectorAll(".my-gallery__item");
 
-    // Показываем выбранную фотографию
+    // находим тригер открытия
     galleryItems.forEach((item, index) => {
       if (index === startIndex) {
         item.classList.add("active");
@@ -20,50 +19,86 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    // Показываем галерею
+    // открытие модалки
     gallery.classList.add("active");
+    document.body.style.width = `calc(100vw - ${
+      window.innerWidth - document.documentElement.clientWidth
+    }px)`;
+    document.body.style.overflow = "hidden";
   }
 
-  // Обработчики событий для триггеров
   photoItems.forEach((item, index) => {
     item.addEventListener("click", function () {
       const galleryId = this.getAttribute("data-gallery-open");
-      showGallery(galleryId, index);
+      const gallery = document.querySelector(`[data-gallery="${galleryId}"]`);
+      showGallery(gallery, index);
     });
   });
 
-  // Обработчики событий для кнопок "prev" и "next"
   myGallery.forEach((gallery) => {
     const closeBtn = gallery.querySelector(".my-gallery__close");
     const prevBtn = gallery.querySelector(".my-gallery__prev");
     const nextBtn = gallery.querySelector(".my-gallery__next");
     const galleryItems = gallery.querySelectorAll(".my-gallery__item");
 
-    if (closeBtn) {
-      closeBtn.addEventListener("click", function () {
-        gallery.classList.remove("active");
-      });
-    }
+    // кнопка закрытия
+    closeBtn.addEventListener("click", function () {
+      gallery.classList.remove("active");
+      document.body.style.overflow = "";
+      document.body.style.width = "";
+    });
 
+    // кнопки перелистывания
     if (prevBtn && nextBtn) {
       let currentIndex = 0;
 
-      // Функция для переключения на предыдущую фотографию
       function showPrev() {
         currentIndex =
           (currentIndex - 1 + galleryItems.length) % galleryItems.length;
-        showGallery(gallery.getAttribute("data-gallery"), currentIndex);
+        showGallery(gallery, currentIndex);
       }
 
-      // Функция для переключения на следующую фотографию
       function showNext() {
         currentIndex = (currentIndex + 1) % galleryItems.length;
-        showGallery(gallery.getAttribute("data-gallery"), currentIndex);
+        showGallery(gallery, currentIndex);
       }
 
       prevBtn.addEventListener("click", showPrev);
       nextBtn.addEventListener("click", showNext);
     }
+
+    // функция закрытия
+    function closeModal(e) {
+      if (
+        e.target.classList.contains("my-gallery__close") ||
+        e.target.closest(".my-gallery__close") ||
+        e.target.classList.contains("my-gallery__overlay")
+      ) {
+        gallery.classList.remove("active");
+        document.body.style.overflow = "";
+        document.body.style.width = "";
+      }
+    }
+
+    gallery.addEventListener("click", (e) => {
+      const target = e.target;
+      const isGalleryItem =
+        target.classList.contains("my-gallery__item") ||
+        target.classList.contains("my-gallery__next") ||
+        target.classList.contains("my-gallery__prev");
+
+      if (!isGalleryItem) {
+        closeModal(e);
+      }
+    });
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && gallery.classList.contains("active")) {
+        gallery.classList.remove("active");
+        document.body.style.overflow = "";
+        document.body.style.width = "";
+      }
+    });
   });
 
   //!! End Scripts
